83d7052ab8828c73014f03b769db14223b17ad0b
aggicreative555 <142678571+aggicreative555@users.noreply.github.com>
2025-05-05 16:36:52 +0200

    feat(getDicountInfo): add getDicountInfo function to calculate real price and savings in percent

---
+++ src/utils/getDicountInfo.js
@@ -0,0 +1,13 @@
+export function getDiscountInfo(price, discountedPrice) {
+    if (discountedPrice >= price) {
+        return null;
+    }
+
+    const savings = price - discountedPrice;
+    const percentage = Math.round((savings / price) * 100);
+
+    return {
+        savings,
+        percentage,
+    };
+}
