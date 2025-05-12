48d66b291064c2fbad22bddc13ce7e41d0d28733
aggicreative555 <142678571+aggicreative555@users.noreply.github.com>
2025-05-12 21:47:30 +0200

    feat(searchBar): add debounce timer, add suggestions
    added debounce timer so query doesnt send too often
    added suggestions of top 3 suggestions
    added error toast if no result

--- src/components/search/SearchBar.jsx
+++ src/components/search/SearchBar.jsx
@@ -1,33 +1,91 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { useProductStore } from '../../stores/productStore';
+import { showToast } from '../../utils/toast';
+import { useDebounce } from '../../hooks/useDebounce';
+import { toast } from 'react-toastify';
 
 const SearchBar = ({ onResults }) => {
     const products = useProductStore((state) => state.products);
     const [query, setQuery] = useState('');
-    const [filtered, setFiltered] = useState([]);
+    const debouncedQuery = useDebounce(query, 300);
+    const [suggestions, setSuggestions] = useState([]);
+    const [isFocused, setIsFocused] = useState(false);
+    const wrapperRef = useRef(null);
+    const errorToast = useRef(false);
+    
+    // dismiss error on empty search field
+    toast.dismiss(showToast.error);
 
 
     useEffect(() => {
-        if (query.length === 0 || query.length > 2) {
-            const results = products.filter((product) => product.title.toLowerCase().includes(query.toLowerCase));
-    
-            setFiltered(results);
-    
-            if (onResults) onResults(results); 
+      
+      if (debouncedQuery.length === 0 || debouncedQuery.length > 1 ) {
+        const results = products.filter((product) => product.title.toLowerCase().includes(debouncedQuery.toLowerCase()));
+        
+        // 1 error toast per second
+        if (debouncedQuery.length > 1 && results.length === 0 ) {
+          showToast.error('No items match your search. Please try again.');
+          errorToast.current = true;
+        } else {
+          setTimeout(() => {
+            errorToast.current = false;
+          },1000);
         }
-    }, [query, products, onResults]);
 
+        setSuggestions(results.slice(0,3));
+
+        if (onResults) {
+          onResults(results)
+        } else {
+          setSuggestions([]);
+          onResults?.(products);
+        }; 
+      }
+    }, [debouncedQuery, products, onResults]);
+
+    useEffect(() => {
+      const handleClickOutside = (event) => {
+        if (wrapperRef.current && !wrapperRef.current.contains(event.target))
+          setIsFocused(false);
+      }
+      document.addEventListener('mousedown', handleClickOutside);
+
+      return () => {
+        document.removeEventListener('mousedown', handleClickOutside);
+      }
+    }, []);
+
+    const handleSuggestions = (title) => {
+      setQuery(title);
+      setSuggestions([]);
+    }
 
   return (
-    <div className="w-full max-w-xl mx-auto my-4">
+    <div ref={wrapperRef}
+    className="w-full max-w-xl mx-auto my-4 relative z-10">
     <input
       type="search"
       placeholder="Search for products..."
-      className="w-full p-3 border rounded shadow-sm focus:outline-none focus:ring-2 focus:ring-amber-600"
+      className="w-full p-3 border rounded shadow-sm focus:outline-none focus:ring-2 focus:ring-black"
       onChange={(e) => setQuery(e.target.value)}
       value={query}
       aria-label="Search products"
+      onFocus={() => setIsFocused(true)}
     />
+
+    {isFocused && suggestions.length > 0 && (
+      <ul className='absolute left-0 right-0 mt-1 py-2 bg-white border rounded shadow z-10 max-h-60 overflow-y-auto'>
+        {suggestions.map((product) => (
+          <li key={product.id}
+          className='px-6 py-2 hover:bg-gray-300 cursor-pointer'
+          onClick={() => handleSuggestions(product.title)}
+          >
+            {product.title}
+          </li>
+        ))}
+
+      </ul>
+    )}
   </div>
   )
 }
