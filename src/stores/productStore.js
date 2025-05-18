167cb9449f2efcffed3d2c4e61d1f4499e70af2d
aggicreative555 <142678571+aggicreative555@users.noreply.github.com>
2025-05-18 19:43:16 +0200

    fix(console.log): remove console.logs

--- src/stores/productStore.js
+++ src/stores/productStore.js
@@ -12,8 +12,6 @@ export const useProductStore = create((set) => ({
       const response = await fetch('https://v2.api.noroff.dev/online-shop');
       const json = await response.json();
 
-      console.log('API response:', json);
-
       set({ products: json.data, isLoading: false });
     } catch (error) {
       console.error('Fetch error:', error);
